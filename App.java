a19a0f270a88ea941aa136c405155d2bad637483
Tom-Sui <jinzhousui@gmail.com>
2025-03-21 15:07:13 +0800

    Test Update Package

--- App.java
+++ App.java
@@ -1,5 +1,9 @@
+import PackageA.PackageA;
+
 public class App {
     public static void main(String[] args) {
         // Main codes
+        PackageA packageA = new PackageA();
+        packageA.SomeFunction();
     }
 }
---
+++ PackageA/PackageA.java
@@ -0,0 +1,12 @@
+package PackageA;
+
+public class PackageA {
+
+    private String string = new String("This is a package");
+    public PackageA(){
+        System.out.println(string);
+    }
+    public void SomeFunction(){
+        System.out.println("This is a function");
+    }
+}

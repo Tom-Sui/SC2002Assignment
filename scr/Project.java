2ca2d50b8b0d7c3e25bd7ba346b68b999c6752cf
Tom-Sui <jinzhousui@gmail.com>
2025-04-02 00:54:48 +0800

    changed all java files to folder scr

--- scr/Project.java
+++ scr/Project.java
@@ -13,7 +13,7 @@ public class Project {
     private Applicant[] applicants;
     private HDBOfficer[] hdbOfficers;
     private Enquiry[] enquiry;
-    private MaritialStauts maritialStauts;
+    private MaritialStatus maritialStauts;
 
     //Wasn't stated in the UML diagram
     private FlateType flateType;

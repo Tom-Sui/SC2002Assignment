b07157f7646c3d26a175c143f4b3bf480524420e
ZachryHRQ <zac.hong01@gmail.com>
2025-04-05 12:27:56 +0800

    fix: changed the user flow

--- scr/ApplicantApp.java
+++ scr/ApplicantApp.java
@@ -29,21 +29,24 @@ public class ApplicantApp {
 			System.out.printf("-1. Exit\n");
 			System.out.printf("Enter your choice: ");
 			choice = sc.nextInt();
-			
 
 			if (choice == 1) {
 				applicant.viewAvailableProjects(projectList);
 			}
 			else if (choice == 2) {
+				/* 
+				Check if the applicant has already applied for a project
+				If yes, then print the message and break the loop	
+				Else, print the list of applied projects and ask the applicant to enter the project number to apply for
+				*/
+				if (applicant.getAppliedProject() != null){
+					System.out.println("Cannot apply for project, already applied for a project: " + applicant.getAppliedProject().getProjectName());
+					break;
+				}
 				applicant.viewAppliedProjects(projectList);
 				System.out.printf("Enter the project number to apply for: ");	
 				int projectNumber = sc.nextInt();
-				if (applicant.getAppliedProject() == null){
-					applicant.applyForProject(projectList.get(projectNumber-1));
-				}
-				else{
-					System.out.println("Cannot apply for project, already applied for a project: " + applicant.getAppliedProject().getProjectName());
-				}
+				applicant.applyForProject(projectList.get(projectNumber-1));
 			}
 			
 		} while (choice != -1);

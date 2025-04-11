9f9a6712559208880537a0272836597b8c159087
Tom-Sui <jinzhousui@gmail.com>
2025-04-11 20:31:15 +0800

    Added code to revoe project, add user index, add visibility in txt

--- src/App.java
+++ src/App.java
@@ -49,10 +49,10 @@ public class App{
 
 
         //Initialize manager managed projects
-        hdbManagers = init.setManagerManagedProjects(hdbManagers,projects);
-        System.out.println(hdbManagers.get(0).getName());
-        hdbManagers.get(0).deletProject(projects.get(0));
-        System.exit(0);
+        // hdbManagers = init.setManagerManagedProjects(hdbManagers,projects);
+        // System.out.println(hdbManagers.get(0).getName());
+        // hdbManagers.get(0).deletProject(projects.get(0));
+        // System.exit(0);
         //Example of how to edit project
         // hdbManagers.get(1).editProject("new name", "different name", "projectname",hdbManagers,hdbOfficers);
 

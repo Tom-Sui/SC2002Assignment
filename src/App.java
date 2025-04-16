faa125b2be73cec1cbe59da711e4690b0a154ca1
Qnweii <QWONG011@e.ntu.edu.sg>
2025-04-16 14:10:39 +0800

    comment out test case

--- src/App.java
+++ src/App.java
@@ -58,10 +58,10 @@ public class App{
 
 
 
-        General general = new General();
-        general.editProjectFile(projects.get(1),"New Name");
-        projects.get(1).setProjectName("New Name");
-        System.exit(0);
+        //General general = new General();
+        //general.editProjectFile(projects.get(1),"New Name");
+        //projects.get(1).setProjectName("New Name");
+        //System.exit(0);
         
         // System.out.println(hdbManagers.get(0).getName());
         // hdbManagers.get(0).deletProject(projects.get(0));
